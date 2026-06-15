783cb181060345ec246deef4886d2bf02158daeb
James <james.m.topmiller@gmail.com>
2026-06-15 12:52:51 -0700

    add homepage <p>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,21 @@
 export default function Home() {
   return (
     <div className="home">
-      <h2>Explore the History of Kings Island's Roller Coasters</h2>
+      <h2>About Kings Island</h2>
+      <p>
+        Kings Island is a premier amusement and water park located in Mason, Ohio, near Cincinnati.
+        Since opening in 1972, it has become one of the Midwest's most popular family destinations, featuring world-class roller coasters, family attractions, live entertainment, and the Soak City Water Park.
+        Known for iconic rides like The Beast and Orion, Kings Island offers memorable experiences for thrill-seekers, families, and visitors of all ages.
+        It is is one of the largest and most famous seasonal amusement parks in the United States.
+        The park is currently operated by Six Flags Entertainment Corporation.
+      </p>
 
-      {/* <div className="menu-grid">
-        <Link to="/coasters">Coasters</Link>
-        <Link to="/timeline">Timeline</Link>
-        <Link to="/decades">Decades</Link>
-        <Link to="/records">Records</Link>
-      </div> */}
+      <h3>Explore the History of Kings Island's Roller Coasters above...</h3>
+
+      <p>
+        Kings Island features eight distinct themed areas—including International Street, Adventure Port, Action Zone, Area 72, Coney Mall, Rivertown, Planet Snoopy, and Soak City Water Park.
+      </p>
+
     </div>
   );
 }
